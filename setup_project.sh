bc56dbf8df8b3eabe1ecbaf0ece59412b9735437
nhonphan001 <33338493+nhonphan001@users.noreply.github.com>
2026-09-07 16:38:38 +0700

    Create setup_project.sh

---
+++ setup_project.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Chạy trong terminal của GitHub Codespaces:
+#   bash setup_project.sh
+# Sau đó:
+#   git add . && git commit -m "init" && git push
+set -e
+echo "Đang giải nén project..."
+base64 -d project.b64 > project.tar.gz
+tar xzf project.tar.gz
+rm project.tar.gz
+echo "Xong! Xem thư mục FamilyConnect-android/ và dashboard/"
